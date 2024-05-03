ba12320b4bb07f6ddc6efaf8035556fbad5f708f
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-03 15:09:54 +0200

    add Logs navbar button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,5 @@
 import { Button } from "./ui/button";
-import { Network } from "lucide-react";
+import { Network, Terminal } from "lucide-react";
 import { useHostCount } from "@/lan/use-host-count";
 import ThemeToggle from "./theme-toggle";
 
@@ -7,17 +7,29 @@ export default function Navbar() {
   const { hostCount } = useHostCount();
 
   return (
-    <div className="container mx-auto flex justify-between pt-5">
-      <Button variant="outline" className="relative flex h-16 w-16 flex-col">
-        <Network size={20} />
-        <p>LAN</p>
-        {hostCount > 0 && (
-          <div className="absolute right-1 top-1 rounded-full bg-gray-400 px-[6px] py-[2px] text-xs text-white">
-            {hostCount}
-          </div>
-        )}
-      </Button>
+    <nav className="container mx-auto mb-2 flex justify-between pt-5">
+      <div className="flex gap-4">
+        <Button
+          variant="outline"
+          className="relative flex h-12 w-12 flex-col p-0 text-muted-foreground"
+        >
+          <Network size={20} />
+          <p>LAN</p>
+          {hostCount > 0 && (
+            <div className="absolute right-1 top-1 rounded-full bg-gray-400 px-[6px] py-[2px] text-xs text-white">
+              {hostCount}
+            </div>
+          )}
+        </Button>
+        <Button
+          variant="outline"
+          className="flex h-12 w-12 flex-col p-0 text-muted-foreground"
+        >
+          <Terminal size={20} />
+          <p>Logs</p>
+        </Button>
+      </div>
       <ThemeToggle />
-    </div>
+    </nav>
   );
 }
