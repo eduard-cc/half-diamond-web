0abcdef476b47c3a7a205f780a5b4c2f518e52b9
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-07 19:40:49 +0200

    remove color variables

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -17,13 +17,8 @@ const Toaster = ({ ...props }: ToasterProps) => {
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
           cancelButton:
             "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
-          success:
-            "group toast group-[.toaster]:bg-success-foreground dark:group-[.toaster]:bg-success group-[.toaster]:text-success dark:group-[.toaster]:text-success-foreground group-[.toaster]:shadow-lg",
           error:
             "group toast group-[.toaster]:bg-destructive-foreground dark:group-[.toaster]:bg-destructive group-[.toaster]:text-destructive dark:group-[.toaster]:text-destructive-foreground group-[.toaster]:shadow-lg",
-          info: "group toast group-[.toaster]:bg-info-foreground dark:group-[.toaster]:bg-info group-[.toaster]:text-info dark:group-[.toaster]:text-info-foreground group-[.toaster]:shadow-lg",
-          warning:
-            "group toast group-[.toaster]:bg-warning-foreground dark:group-[.toaster]:bg-warning group-[.toaster]:text-warning dark:group-[.toaster]:text-warning-foreground group-[.toaster]:shadow-lg",
         },
       }}
       {...props}
