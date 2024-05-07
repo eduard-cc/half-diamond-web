62b31c9f8c69cb30f93aba62fec0ff1c35ddf6c7
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-07 02:22:48 +0200

    refactor hook to support different port scan types

--- src/lan/use-scan-ports.ts
+++ src/lan/use-scan-ports.ts
@@ -1,33 +1,38 @@
 import { useState, useCallback } from "react";
+import { PortScanType } from "./types";
 
 export default function useScanPorts() {
   const [data, setData] = useState<Record<string, string> | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
 
-  const scanPorts = useCallback(async (targetIps: string[]) => {
-    setLoading(true);
-    setError(null);
-    try {
-      console.log("about to scan ports");
-      const response = await fetch("http://localhost:8000/ports", {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(targetIps),
-      });
-      console.log("finished scanning ports");
-      // Don't need the data since we're polling for it anyway
-      const openPorts = await response.json();
-      setData(openPorts);
-    } catch (error) {
-      console.error(error);
-      setError(error as Error);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+  const scanPorts = useCallback(
+    async (targetIps: string[], scanType: PortScanType) => {
+      setLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(
+          `http://localhost:8000/ports?scan_type=${scanType}`,
+          {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(targetIps),
+          },
+        );
+        const openPorts = await response.json();
+        console.log(openPorts);
+        setData(openPorts);
+      } catch (error) {
+        console.error(error);
+        setError(error as Error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [],
+  );
 
   return { loading, error, data, scanPorts };
 }
