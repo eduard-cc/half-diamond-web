aa05e9780b3c8ecb44fa6aa151e6a106337e7b10
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-15 18:34:21 +0200

    add EventsTable

--- src/logs/page.tsx
+++ src/logs/page.tsx
@@ -1,3 +1,25 @@
-export default function Logs() {
-  return <p>logs</p>;
+import { columns } from "./logs-table/columns";
+import { EventsTable } from "./logs-table/log-table";
+import useFetchEvents from "./hooks/use-fetch-events";
+import type { Event } from "@/lan/types";
+
+type EventsPageProps = {
+  events: Event[];
+  setEvents: React.Dispatch<React.SetStateAction<Event[]>>;
+};
+
+export default function EventsPage({ events, setEvents }: EventsPageProps) {
+  const { isPending: fetchIsPending, error: fetchError } =
+    useFetchEvents(setEvents);
+
+  return (
+    <div className="container mx-auto py-2">
+      <EventsTable
+        columns={columns}
+        data={events}
+        isPending={fetchIsPending}
+        error={fetchError}
+      />
+    </div>
+  );
 }
