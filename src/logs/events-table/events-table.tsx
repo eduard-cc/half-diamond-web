dcb0a740964a99e05302fc15583af00185ca1f24
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-15 21:08:57 +0200

    add faceted filter component

---
+++ src/logs/events-table/events-table.tsx
@@ -0,0 +1,173 @@
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useEffect, useState } from "react";
+import type { Event } from "@/lan/types";
+import { DataTableColumnToggle } from "@/lan/hosts-table/data-table-column-toggle";
+import DataTablePaginationButtons from "@/lan/hosts-table/data-table-pagination-buttons";
+import { CircleAlert, X } from "lucide-react";
+import { DataTableFacetedFilter } from "./events-table-faceted-filter";
+import { Button } from "@/components/ui/button";
+
+type EventsTableProps<TData extends Event, TValue> = {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+  isPending: boolean;
+  error: Error | null;
+};
+
+export function EventsTable<TData extends Event, TValue>({
+  columns,
+  data,
+  isPending,
+  error,
+}: EventsTableProps<TData, TValue>) {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [rowSelection, setRowSelection] = useState({});
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>(
+    () => {
+      const savedVisibility = window.sessionStorage.getItem("columnVisibility");
+      return savedVisibility ? JSON.parse(savedVisibility) : {};
+    },
+  );
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    onRowSelectionChange: setRowSelection,
+    onColumnVisibilityChange: setColumnVisibility,
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting,
+      rowSelection,
+      columnVisibility: columnVisibility,
+      columnFilters,
+    },
+  });
+
+  const isFiltered = table.getState().columnFilters.length > 0;
+
+  // Save the user's column visibility preferences in session storage
+  useEffect(() => {
+    window.sessionStorage.setItem(
+      "columnVisibility",
+      JSON.stringify(columnVisibility),
+    );
+  }, [columnVisibility]);
+
+  return (
+    <>
+      <div className="mb-2 flex justify-between">
+        <DataTableFacetedFilter column={table.getColumn("type")} title="Type" />
+        {isFiltered && (
+          <Button
+            variant="ghost"
+            onClick={() => table.resetColumnFilters()}
+            className="h-8 px-2 lg:px-3"
+          >
+            Reset
+            <X className="ml-2 h-4 w-4" />
+          </Button>
+        )}
+        <DataTableColumnToggle table={table} />
+      </div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {error ? (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center font-semibold"
+                >
+                  <div className="flex items-center justify-center text-destructive">
+                    <p>
+                      <CircleAlert className="mr-1 h-4 w-4" />
+                    </p>
+                    <span>Failed to establish connection to API.</span>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ) : isPending ? (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  Loading...
+                </TableCell>
+              </TableRow>
+            ) : table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id} className="max-w-64">
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No events found.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <DataTablePaginationButtons table={table} />
+    </>
+  );
+}
