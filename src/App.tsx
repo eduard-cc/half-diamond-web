6b4e837f8984380e62b739ee7fa37108d518e9d0
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-15 18:32:47 +0200

    use hosts and events providers in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,39 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "@/components/navbar";
 import { Toaster } from "@/components/ui/toaster";
 import { HostCountProvider } from "@/lan/hooks/host-count-provider";
-import Hosts from "@/lan/page";
+import HostsPage from "@/lan/page";
 import { ThemeProvider } from "@/providers/theme-provider";
-import Logs from "@/logs/page";
+import useWebSocket from "@/lan/hooks/use-websocket";
+import { HostsProvider, useHosts } from "@/providers/hosts-provider";
+import { EventsProvider, useEvents } from "@/providers/events-provider";
+import EventsPage from "@/logs/page";
 
 export default function App() {
+  const { hosts, setHosts } = useHosts();
+  const { events, setEvents } = useEvents();
+  useWebSocket(setHosts, setEvents);
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <HostCountProvider>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Hosts />} />
-            <Route path="/logs" element={<Logs />} />
-          </Routes>
-        </Router>
-      </HostCountProvider>
+      <HostsProvider>
+        <EventsProvider>
+          <HostCountProvider>
+            <Router>
+              <Navbar />
+              <Routes>
+                <Route
+                  path="/"
+                  element={<HostsPage hosts={hosts} setHosts={setHosts} />}
+                />
+                <Route
+                  path="/events"
+                  element={<EventsPage events={events} setEvents={setEvents} />}
+                />
+              </Routes>
+            </Router>
+          </HostCountProvider>
+        </EventsProvider>
+      </HostsProvider>
       <Toaster />
     </ThemeProvider>
   );
