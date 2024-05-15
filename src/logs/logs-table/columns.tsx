0e1942fae7cdbd15afa8848cc97e711efdd8be76
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-15 18:36:56 +0200

    format date using toLocaleString and in tooltip with date-fns

--- src/logs/logs-table/columns.tsx
+++ src/logs/logs-table/columns.tsx
@@ -1,10 +1,32 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { Event } from "@/lan/types";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { formatDistanceToNow } from "date-fns";
 
-export const columns = (): ColumnDef<Event>[] => [
+export const columns: ColumnDef<Event>[] = [
   {
     accessorKey: "time",
     header: "Time",
+    cell: ({ row }) => {
+      const date = new Date(row.original.time);
+      const formattedDate = formatDistanceToNow(date, {
+        addSuffix: true,
+        includeSeconds: true,
+      });
+      return (
+        <TooltipProvider delayDuration={0}>
+          <Tooltip>
+            <TooltipTrigger>{date.toLocaleString()}</TooltipTrigger>
+            <TooltipContent>{formattedDate}</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      );
+    },
   },
   {
     accessorKey: "type",
