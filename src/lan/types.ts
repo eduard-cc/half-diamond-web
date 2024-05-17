ccef8e1834744ca2afb20bd8c36f4fde015e80b0
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-17 15:03:28 +0200

    fix order of scans in type

--- src/lan/types.ts
+++ src/lan/types.ts
@@ -38,8 +38,8 @@ export enum EventType {
   HOST_SEEN = "host.seen",
   HOST_CONNECTED = "host.connected",
   HOST_DISCONNECTED = "host.disconnected",
-  SCAN_TCP = "scan.tcp",
   SCAN_SYN = "scan.syn",
+  SCAN_TCP = "scan.tcp",
   SCAN_UDP = "scan.udp",
   OS_DETECTED = "os.detected",
 }
