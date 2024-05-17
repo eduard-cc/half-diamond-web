64c00428f7e2ff6c234e1084a526c46988b4b854
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-17 17:11:01 +0200

    add scrollbar

--- src/logs/events-table/events-table-faceted-filter.tsx
+++ src/logs/events-table/events-table-faceted-filter.tsx
@@ -19,11 +19,23 @@ import {
 } from "@/components/ui/popover";
 import { Separator } from "@/components/ui/separator";
 import { EventType } from "@/lan/types";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface DataTableFacetedFilterProps<TData, TValue> {
   column?: Column<TData, TValue>;
   title?: string;
 }
+
+const eventGroups = {
+  "Host Events": [
+    EventType.HOST_NEW,
+    EventType.HOST_SEEN,
+    EventType.HOST_CONNECTED,
+    EventType.HOST_DISCONNECTED,
+  ],
+  "Scan Events": [EventType.SCAN_SYN, EventType.SCAN_TCP, EventType.SCAN_UDP],
+  "OS Events": [EventType.OS_DETECTED],
+};
 
 export function DataTableFacetedFilter<TData, TValue>({
   column,
@@ -78,61 +90,73 @@ export function DataTableFacetedFilter<TData, TValue>({
       </PopoverTrigger>
       <PopoverContent className="w-[200px] p-0" align="start">
         <Command>
-          <CommandInput placeholder={title} />
-          <CommandList className="max-h-96">
-            <CommandEmpty>No results found.</CommandEmpty>
-            <CommandGroup>
-              {options.map((option) => {
-                const isSelected = selectedValues.has(option);
-                return (
-                  <CommandItem
-                    key={option}
-                    onSelect={() => {
-                      if (isSelected) {
-                        selectedValues.delete(option);
-                      } else {
-                        selectedValues.add(option);
-                      }
-                      const filterValues = Array.from(selectedValues);
-                      column?.setFilterValue(
-                        filterValues.length ? filterValues : undefined,
-                      );
-                    }}
-                  >
-                    <div
-                      className={cn(
-                        "mr-2 flex h-4 w-4 items-center justify-center rounded-sm border border-primary",
-                        isSelected
-                          ? "bg-primary text-primary-foreground"
-                          : "opacity-50 [&_svg]:invisible",
-                      )}
-                    >
-                      <Check className={cn("h-4 w-4")} />
-                    </div>
-                    <span>{option}</span>
-                    {facets?.get(option) && (
-                      <span className="ml-auto flex h-4 w-4 items-center justify-center font-mono text-xs">
-                        {facets.get(option)}
-                      </span>
+          <CommandInput placeholder="Search event type" className="h-10" />
+          <ScrollArea className="h-72">
+            <CommandList className="max-h-full pr-2">
+              <CommandEmpty>No results found.</CommandEmpty>
+              {Object.entries(eventGroups).map(
+                ([heading, eventTypes], index, array) => (
+                  <>
+                    <CommandGroup heading={heading} key={heading}>
+                      {eventTypes.map((eventType) => {
+                        const option = eventType;
+                        const isSelected = selectedValues.has(option);
+                        return (
+                          <CommandItem
+                            key={option}
+                            onSelect={() => {
+                              if (isSelected) {
+                                selectedValues.delete(option);
+                              } else {
+                                selectedValues.add(option);
+                              }
+                              const filterValues = Array.from(selectedValues);
+                              column?.setFilterValue(
+                                filterValues.length ? filterValues : undefined,
+                              );
+                            }}
+                          >
+                            <div
+                              className={cn(
+                                "mr-2 flex h-4 w-4 items-center justify-center rounded-sm border border-primary",
+                                isSelected
+                                  ? "bg-primary text-primary-foreground"
+                                  : "opacity-50 [&_svg]:invisible",
+                              )}
+                            >
+                              <Check className={cn("h-4 w-4")} />
+                            </div>
+                            <span>{option}</span>
+                            {facets?.get(option) && (
+                              <span className="ml-auto flex h-4 w-4 items-center justify-center font-mono text-xs">
+                                {facets.get(option)}
+                              </span>
+                            )}
+                          </CommandItem>
+                        );
+                      })}
+                    </CommandGroup>
+                    {index < array.length - 1 && (
+                      <CommandSeparator className="mr-1" />
                     )}
-                  </CommandItem>
-                );
-              })}
-            </CommandGroup>
-            {selectedValues.size > 0 && (
-              <>
-                <CommandSeparator />
-                <CommandGroup>
-                  <CommandItem
-                    onSelect={() => column?.setFilterValue(undefined)}
-                    className="justify-center text-center"
-                  >
-                    Clear filters
-                  </CommandItem>
-                </CommandGroup>
-              </>
-            )}
-          </CommandList>
+                  </>
+                ),
+              )}
+            </CommandList>
+          </ScrollArea>
+          {selectedValues.size > 0 && (
+            <CommandList>
+              <CommandSeparator />
+              <CommandGroup>
+                <CommandItem
+                  onSelect={() => column?.setFilterValue(undefined)}
+                  className="justify-center text-center"
+                >
+                  Clear filters
+                </CommandItem>
+              </CommandGroup>
+            </CommandList>
+          )}
         </Command>
       </PopoverContent>
     </Popover>
