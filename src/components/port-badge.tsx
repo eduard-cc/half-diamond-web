0d9e8793b93112f093029b031c9915e7aaead2d7
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-17 13:36:52 +0200

    extract PortBadge into a component

---
+++ src/components/port-badge.tsx
@@ -0,0 +1,50 @@
+import { Port } from "@/lan/types";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+import { Badge } from "./ui/badge";
+
+export default function PortBadge({ port }: { port: Port }) {
+  return (
+    <TooltipProvider key={port.port}>
+      <Tooltip>
+        <TooltipTrigger>
+          <Badge variant="secondary">{port.port}</Badge>
+        </TooltipTrigger>
+        <TooltipContent>
+          <div className="grid">
+            <div className="inline">
+              {port.port}/{port.protocol}
+            </div>
+            {port.name && (
+              <div className="inline text-muted-foreground">{port.name}</div>
+            )}
+            {port.product && (
+              <div className="inline text-muted-foreground">
+                {port.product} {port.version ? `(${port.version})` : ""}
+              </div>
+            )}
+            {port.extrainfo && (
+              <div className="inline text-muted-foreground">
+                {port.extrainfo}
+              </div>
+            )}
+            {port.reason && (
+              <div className="inline uppercase text-muted-foreground">
+                {port.reason}
+              </div>
+            )}
+            {port.conf && (
+              <div className="inline text-muted-foreground">
+                {port.conf}/10 conf
+              </div>
+            )}
+          </div>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
--- src/lan/hosts-table/columns.tsx
+++ src/lan/hosts-table/columns.tsx
@@ -11,6 +11,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Button } from "@/components/ui/button";
 import CopyToClipboardButton from "@/components/copy-to-clipboard-button";
 import { getFormattedDate } from "@/lib/get-formatted-date";
+import PortBadge from "@/components/port-badge";
 
 export const columns = (
   detectOs: (targetIps: string[]) => void,
@@ -163,46 +164,7 @@ export const columns = (
         return (
           <div className="flex flex-wrap gap-1">
             {openPorts.map((port) => (
-              <TooltipProvider key={port.port}>
-                <Tooltip>
-                  <TooltipTrigger>
-                    <Badge variant="secondary">{port.port}</Badge>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <div className="grid">
-                      <div className="inline">
-                        {port.port}/{port.protocol}
-                      </div>
-                      {port.name && (
-                        <div className="inline text-muted-foreground">
-                          {port.name}
-                        </div>
-                      )}
-                      {port.product && (
-                        <div className="inline text-muted-foreground">
-                          {port.product}{" "}
-                          {port.version ? `(${port.version})` : ""}
-                        </div>
-                      )}
-                      {port.extrainfo && (
-                        <div className="inline text-muted-foreground">
-                          {port.extrainfo}
-                        </div>
-                      )}
-                      {port.reason && (
-                        <div className="inline uppercase text-muted-foreground">
-                          {port.reason}
-                        </div>
-                      )}
-                      {port.conf && (
-                        <div className="inline text-muted-foreground">
-                          {port.conf}/10 conf
-                        </div>
-                      )}
-                    </div>
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
+              <PortBadge key={port.port} port={port} />
             ))}
           </div>
         );
