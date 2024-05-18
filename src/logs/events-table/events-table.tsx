6ea0d8c2c69cd595910b249502895ac5bbe89064
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-18 12:25:28 +0200

    remove pagination and column toggling and make events table scrollable

--- src/logs/events-table/events-table.tsx
+++ src/logs/events-table/events-table.tsx
@@ -2,11 +2,9 @@ import {
   ColumnDef,
   ColumnFiltersState,
   SortingState,
-  VisibilityState,
   flexRender,
   getCoreRowModel,
   getFilteredRowModel,
-  getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
@@ -18,13 +16,12 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { useEffect, useState } from "react";
-import { DataTableColumnToggle } from "@/lan/hosts-table/data-table-column-toggle";
-import DataTablePaginationButtons from "@/lan/hosts-table/data-table-pagination-buttons";
+import { useState } from "react";
 import { CircleAlert, X } from "lucide-react";
 import { DataTableFacetedFilter } from "./events-table-faceted-filter";
 import { Button } from "@/components/ui/button";
 import { Event } from "@/lan/types";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 type EventsTableProps<TData extends Event, TValue> = {
   columns: ColumnDef<TData, TValue>[];
@@ -42,20 +39,12 @@ export function EventsTable<TData extends Event, TValue>({
   const [sorting, setSorting] = useState<SortingState>([]);
   const [rowSelection, setRowSelection] = useState({});
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
-  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>(
-    () => {
-      const savedVisibility = window.sessionStorage.getItem("columnVisibility");
-      return savedVisibility ? JSON.parse(savedVisibility) : {};
-    },
-  );
 
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
     onRowSelectionChange: setRowSelection,
-    onColumnVisibilityChange: setColumnVisibility,
-    getPaginationRowModel: getPaginationRowModel(),
     onSortingChange: setSorting,
     getSortedRowModel: getSortedRowModel(),
     onColumnFiltersChange: setColumnFilters,
@@ -63,20 +52,11 @@ export function EventsTable<TData extends Event, TValue>({
     state: {
       sorting,
       rowSelection,
-      columnVisibility: columnVisibility,
       columnFilters,
     },
   });
 
   const isFiltered = table.getState().columnFilters.length > 0;
-
-  // Save the user's column visibility preferences in session storage
-  useEffect(() => {
-    window.sessionStorage.setItem(
-      "columnVisibility",
-      JSON.stringify(columnVisibility),
-    );
-  }, [columnVisibility]);
 
   return (
     <>
@@ -92,82 +72,82 @@ export function EventsTable<TData extends Event, TValue>({
             <X className="ml-2 h-4 w-4" />
           </Button>
         )}
-        <DataTableColumnToggle table={table} />
       </div>
       <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  return (
-                    <TableHead key={header.id}>
-                      {header.isPlaceholder
-                        ? null
-                        : flexRender(
-                            header.column.columnDef.header,
-                            header.getContext(),
-                          )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {error ? (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center font-semibold"
-                >
-                  <div className="flex items-center justify-center text-destructive">
-                    <p>
-                      <CircleAlert className="mr-1 h-4 w-4" />
-                    </p>
-                    <span>Failed to establish connection to API.</span>
-                  </div>
-                </TableCell>
-              </TableRow>
-            ) : isPending ? (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  Loading...
-                </TableCell>
-              </TableRow>
-            ) : table.getRowModel().rows?.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow
-                  key={row.id}
-                  data-state={row.getIsSelected() && "selected"}
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id} className="max-w-64">
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext(),
-                      )}
-                    </TableCell>
-                  ))}
+        <ScrollArea className="h-[40rem]">
+          <Table>
+            <TableHeader className="sticky top-0 border-b bg-background">
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext(),
+                            )}
+                      </TableHead>
+                    );
+                  })}
                 </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No events found.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {error ? (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center font-semibold"
+                  >
+                    <div className="flex items-center justify-center text-destructive">
+                      <p>
+                        <CircleAlert className="mr-1 h-4 w-4" />
+                      </p>
+                      <span>Failed to establish connection to API.</span>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ) : isPending ? (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    Loading...
+                  </TableCell>
+                </TableRow>
+              ) : table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id} className="max-w-64">
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No events found.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </ScrollArea>
       </div>
-      <DataTablePaginationButtons table={table} />
     </>
   );
 }
