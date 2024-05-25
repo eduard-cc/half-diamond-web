ed8209d29e018588901598a022f043e30e73aa8f
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-25 14:14:30 +0200

    add dropdown menu for mobile

--- src/routes/lan/modules/module-launcher-toolbar.tsx
+++ src/routes/lan/modules/module-launcher-toolbar.tsx
@@ -4,6 +4,15 @@ import type { Host } from "@/types/host";
 import { useHosts } from "@/providers/hosts-provider";
 import { useCallback, useMemo } from "react";
 import { ArpSpoofDialog } from "./arp-spoof-dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { ChevronDown } from "lucide-react";
 
 type ModuleLauncherToolbarProps = {
   monitor: Module;
@@ -46,25 +55,64 @@ export default function ModuleLauncherToolbar({
 
   return (
     <>
-      <ModuleLauncherButton
-        module={monitor}
-        onClick={handleMonitorClick}
-        moduleName="Monitor"
-      />
-      <ModuleLauncherButton
-        module={probe}
-        onClick={handleProbeClick}
-        moduleName="Probe"
-        monitorIsRunning={monitor.isRunning}
-      />
-      <ArpSpoofDialog
-        arpSpoof={arpSpoof}
-        ips={ips}
-        gatewayIp={gatewayIp}
-        hostIp={hostIp}
-        handleClick={handleArpSpoofClick}
-        targetIps={validTargetIps}
-      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild className="flex xl:hidden">
+          <Button variant="outline" size="sm" className="h-8">
+            Modules
+            <ChevronDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="start">
+          <DropdownMenuGroup className="flex flex-col gap-1">
+            <DropdownMenuItem asChild>
+              <ModuleLauncherButton
+                module={monitor}
+                onClick={handleMonitorClick}
+                moduleName="Monitor"
+              />
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <ModuleLauncherButton
+                module={probe}
+                onClick={handleProbeClick}
+                moduleName="Probe"
+                monitorIsRunning={monitor.isRunning}
+              />
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <ArpSpoofDialog
+                arpSpoof={arpSpoof}
+                ips={ips}
+                gatewayIp={gatewayIp}
+                hostIp={hostIp}
+                handleClick={handleArpSpoofClick}
+                targetIps={validTargetIps}
+              />
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <div className="hidden gap-2 xl:flex">
+        <ModuleLauncherButton
+          module={monitor}
+          onClick={handleMonitorClick}
+          moduleName="Monitor"
+        />
+        <ModuleLauncherButton
+          module={probe}
+          onClick={handleProbeClick}
+          moduleName="Probe"
+          monitorIsRunning={monitor.isRunning}
+        />
+        <ArpSpoofDialog
+          arpSpoof={arpSpoof}
+          ips={ips}
+          gatewayIp={gatewayIp}
+          hostIp={hostIp}
+          handleClick={handleArpSpoofClick}
+          targetIps={validTargetIps}
+        />
+      </div>
     </>
   );
 }
--- src/routes/lan/tasks/task-launcher-toolbar.tsx
+++ src/routes/lan/tasks/task-launcher-toolbar.tsx
@@ -1,6 +1,15 @@
 import TaskLauncherButton from "./task-launcher-button";
 import PortScanTypeDropdown from "./port-scan-type-dropdown";
 import { PortScanType } from "@/types/port-scan-type";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { ChevronDown } from "lucide-react";
 
 type TaskLauncherToolbarProps = {
   targetIps: string[];
@@ -22,32 +31,75 @@ export default function TaskLauncherToolbar({
   setScanType,
 }: TaskLauncherToolbarProps) {
   return (
-    <div className="flex gap-2">
-      <TaskLauncherButton
-        title="Detect OS"
-        onLaunch={(targetIps) => {
-          detectOs(targetIps);
-        }}
-        targetIps={targetIps}
-        pending={osIsPending}
-        loadingText="Detecting OS..."
-      />
-      <div className="flex">
-        <PortScanTypeDropdown
-          disabled={portsIsPending}
-          scanType={scanType}
-          setScanType={setScanType}
-        />
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild className="flex md:hidden">
+          <Button variant="outline" size="sm" className="h-8">
+            Scanners
+            <ChevronDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="start">
+          <DropdownMenuGroup className="flex flex-col gap-1">
+            <DropdownMenuItem asChild>
+              <TaskLauncherButton
+                title="Detect OS"
+                onLaunch={(targetIps) => {
+                  detectOs(targetIps);
+                }}
+                targetIps={targetIps}
+                pending={osIsPending}
+                loadingText="Detecting OS..."
+              />
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <TaskLauncherButton
+                title="Scan Ports"
+                onLaunch={(targetIps) => {
+                  scanPorts(targetIps, scanType);
+                }}
+                targetIps={targetIps}
+                pending={portsIsPending}
+                loadingText="Scanning ports..."
+              />
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+              <PortScanTypeDropdown
+                disabled={portsIsPending}
+                scanType={scanType}
+                setScanType={setScanType}
+              />
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <div className="hidden gap-2 md:flex">
         <TaskLauncherButton
-          title="Scan Ports"
+          title="Detect OS"
           onLaunch={(targetIps) => {
-            scanPorts(targetIps, scanType);
+            detectOs(targetIps);
           }}
           targetIps={targetIps}
-          pending={portsIsPending}
-          loadingText="Scanning ports..."
+          pending={osIsPending}
+          loadingText="Detecting OS..."
         />
+        <div className="flex">
+          <PortScanTypeDropdown
+            disabled={portsIsPending}
+            scanType={scanType}
+            setScanType={setScanType}
+          />
+          <TaskLauncherButton
+            title="Scan Ports"
+            onLaunch={(targetIps) => {
+              scanPorts(targetIps, scanType);
+            }}
+            targetIps={targetIps}
+            pending={portsIsPending}
+            loadingText="Scanning ports..."
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
