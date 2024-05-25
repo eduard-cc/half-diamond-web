0cd21df3527a160a736227f77c1217a25711260a
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-25 21:29:35 +0200

    add tooltip for detect os and scan ports

--- src/routes/lan/tasks/task-launcher-button.tsx
+++ src/routes/lan/tasks/task-launcher-button.tsx
@@ -3,6 +3,12 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/utils/cn";
 import { LoaderCircle } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 type TaskLauncherButtonProps = {
   title: "Detect OS" | "Scan Ports";
@@ -24,57 +30,78 @@ export default function TaskLauncherButton({
   };
 
   return (
-    <Button
-      variant="outline"
-      size="sm"
-      disabled={targetIps.length === 0 || pending}
-      onClick={handleClick}
-      className={cn(
-        "flex w-full items-center justify-between border-transparent md:h-8 md:justify-start md:border-input",
-        title === "Scan Ports" && "rounded-l-none",
-      )}
-    >
-      {pending ? (
-        <>
-          {loadingText}
-          <LoaderCircle className="ml-1 h-4 w-4 animate-spin" />
-        </>
-      ) : (
-        title
-      )}
-      {targetIps.length > 0 && (
-        <>
-          <Separator
-            orientation="vertical"
-            className="mx-2 hidden h-4 md:flex"
-          />
-          <div className="flex space-x-1 md:hidden">
-            <Badge variant="secondary" className="ml-2 rounded-sm px-1">
-              {targetIps.length}
-            </Badge>
-          </div>
-          <div className="hidden space-x-1 md:flex">
-            {targetIps.length > 1 ? (
-              <Badge
-                variant="secondary"
-                className="rounded-sm px-1 font-normal"
-              >
-                {targetIps.length} selected
-              </Badge>
+    <>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              variant="outline"
+              size="sm"
+              disabled={targetIps.length === 0 || pending}
+              onClick={handleClick}
+              className={cn(
+                "flex w-full items-center justify-between border-transparent md:h-8 md:justify-start md:border-input",
+                title === "Scan Ports" && "rounded-l-none",
+              )}
+            >
+              {pending ? (
+                <>
+                  {loadingText}
+                  <LoaderCircle className="ml-1 h-4 w-4 animate-spin" />
+                </>
+              ) : (
+                title
+              )}
+              {targetIps.length > 0 && (
+                <>
+                  <Separator
+                    orientation="vertical"
+                    className="mx-2 hidden h-4 md:flex"
+                  />
+                  <div className="flex space-x-1 md:hidden">
+                    <Badge variant="secondary" className="ml-2 rounded-sm px-1">
+                      {targetIps.length}
+                    </Badge>
+                  </div>
+                  <div className="hidden space-x-1 md:flex">
+                    {targetIps.length > 1 ? (
+                      <Badge
+                        variant="secondary"
+                        className="rounded-sm px-1 font-normal"
+                      >
+                        {targetIps.length} selected
+                      </Badge>
+                    ) : (
+                      targetIps.map((targetIp) => (
+                        <Badge
+                          variant="secondary"
+                          key={targetIp}
+                          className="rounded-sm px-1 font-normal"
+                        >
+                          {targetIp}
+                        </Badge>
+                      ))
+                    )}
+                  </div>
+                </>
+              )}
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent side="bottom" className="max-w-80">
+            {title === "Detect OS" ? (
+              <>
+                <p>Detects the operating system of selected hosts</p>
+                <p>using TCP/IP stack fingerprinting.</p>
+              </>
             ) : (
-              targetIps.map((targetIp) => (
-                <Badge
-                  variant="secondary"
-                  key={targetIp}
-                  className="rounded-sm px-1 font-normal"
-                >
-                  {targetIp}
-                </Badge>
-              ))
+              <>
+                <p>Enumerates open ports of selected hosts</p>
+                <p>using TCP/IP based service discovery.</p>
+              </>
             )}
-          </div>
-        </>
-      )}
-    </Button>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </>
   );
 }
