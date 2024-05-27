720acae09c1a9d53e9414384e8f86a52c5e6d523
eduard-cc <eduard.ciociu22@gmail.com>
2024-05-27 02:44:38 +0200

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
